eff66a010f5fff43fbb2510f2c7cf52b708c8974
llllsc <113428213+llllsc@users.noreply.github.com>
2022-09-13 18:08:59 +0800

    Delete tree.js

--- tree.js
+++
@@ -1,9 +0,0 @@
-class tree {
-    constructor(x, y) {
-        this.tree = document.createElement('div');
-        this.tree.className += `tree`;
-        this.tree.style.left = `${x}px`;
-        this.tree.style.top = `${y}px`;
-        document.querySelector('.map').appendChild(this.tree);
-    }
-}
