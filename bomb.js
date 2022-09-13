29f769695181094421e658a9b430b72bad22c3df
llllsc <113428213+llllsc@users.noreply.github.com>
2022-09-13 18:04:43 +0800

    Delete bomb.js

--- bomb.js
+++
@@ -1,9 +0,0 @@
-class bomb {
-    constructor(x, y) {
-        this.bomb=document.createElement('div');
-        this.bomb.className+='bomb';
-        this.bomb.style.left=x+"px";
-        this.bomb.style.top=y+"px";
-        document.querySelector('.map').appendChild(this.bomb);
-    }
-}
